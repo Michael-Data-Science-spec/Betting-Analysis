9a99b1f5a13dc52981dd5897a4c4ccc205c04d20
Michael-Data-Science-spec <76430924+Michael-Data-Science-spec@users.noreply.github.com>
2021-12-31 04:06:30 +0200

    Create json_doesnt_allow_comments_so_here_they_are.py

---
+++ json_doesnt_allow_comments_so_here_they_are.py
@@ -0,0 +1,19 @@
+"""
+"data": {
+    "UNIQUE_TEAM_ID": {
+        "M": [
+"""
+
+"""
+"name" = "touches"
+"name" = "total_back_zone_pass" // passes on own half???
+"name" = "pen_area_entries" // penalty area entries
+"name" = "total_final_third_passes"
+"name" = "long_pass_own_to_opp_success" // counterattacks
+"name" = "final_third_entries" // basically represents same data as pen_area_entries
+"name" = "total_fwd_zone_pass" // defensive fwd means 1st zone ???
+"name" = "blocked_pass"
+"name" = "interception"
+"name" = "fouled_final_third" // represents agressive teams
+"name" = "total_offside" // counterattacking teams
+"""
